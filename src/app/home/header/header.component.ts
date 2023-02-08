1cf2db0a8ed38c465eb8f3ab19d8368be78bf547
LabchariOussama <oussama.bx31@gmail.com>
2023-02-08 12:57:57 +0100

    modifed:style home, nav

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   public isCollapsed = true;
+  scrolled : boolean = false
+  @HostListener('window:scroll', []) onWindowScroll() {
+    // do some stuff here when the window is scrolled
+    const verticalOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+      if(verticalOffset != 0){
+        this.scrolled = true;
+      }
+  }
+  
 }
